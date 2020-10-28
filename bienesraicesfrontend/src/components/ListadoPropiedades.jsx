a63297c479576557ac4a590cd732b4d2552e5517
Cristian Villota <cristiandavid@Cristians-MacBook-Air.local>
2020-10-27 19:34:01 -0500

    graphql query

---
+++ bienesraicesfrontend/src/components/ListadoPropiedades.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {css} from '@emotion/core';
+
+const ListadoPropiedades = () => {
+  return (
+    <div>
+      <h2>Nuestras Propiedades</h2>
+    </div>
+  );
+}
+
+export default ListadoPropiedades;
---
+++ bienesraicesfrontend/src/pages/nosotros.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const nosotros = () => {
+  return (
+    <Layout>
+      <h1>Nosotros
+      </h1>
+    </Layout>
+  );
+}
+
+export default nosotros;
